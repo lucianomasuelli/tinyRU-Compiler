5d5e9ac67664900035f7d199c1c7b385b6848303
gabriel <gabrielmangione1@gmail.com>
2024-03-14 12:35:47 -0300

    Token inicial

--- src/main/java/tinyru/Token.java
+++ src/main/java/tinyru/Token.java
@@ -1,4 +1,30 @@
 package tinyru;
 
 public class Token {
+
+    private final TokenType type;
+    private final String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "type=" + type +
+                ", lexeme='" + lexeme + '\\'' +
+        '}';
+    }
+
 }
+
---
+++ src/main/java/tinyru/TokenType.java
@@ -0,0 +1,15 @@
+package tinyru;
+
+// Enumeración para tipos de token
+public enum TokenType {
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    OPERATOR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+// Otros tipos de token según sea necesario
+}
+
