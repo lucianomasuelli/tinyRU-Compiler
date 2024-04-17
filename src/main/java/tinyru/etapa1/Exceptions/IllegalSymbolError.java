df2b953af16ea8f4e0e0f9cbf304a6bc99026b12
gabriel <gabrielmangione1@gmail.com>
2024-04-17 15:45:19 -0300

    corregido typo en la descripción

--- src/main/java/tinyru/etapa1/Exceptions/IllegalSymbolError.java
+++ src/main/java/tinyru/etapa1/Exceptions/IllegalSymbolError.java
@@ -6,6 +6,6 @@ package tinyru.etapa1.Exceptions;
 public class IllegalSymbolError extends LexerError {
     public IllegalSymbolError(char c, int line, int column) {
         super("| NÚMERO DE LINEA: " + line + "| NÚMERO DE COLUMNA: " + column
-                + "| DESCRIPCION: " + "Invalid simbol '" + c + "'");
+                + "| DESCRIPCION: " + "Invalid symbol '" + c + "'");
     }
 }
