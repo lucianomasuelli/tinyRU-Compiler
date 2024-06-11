b8286237e200d5016ac215abb721f7bc82f27651
gabriel <gabrielmangione1@gmail.com>
2024-06-11 12:23:07 -0300

    asigna memoria

--- src/main/java/tinyru/etapa4/AST/LiteralNode.java
+++ src/main/java/tinyru/etapa4/AST/LiteralNode.java
@@ -70,7 +70,32 @@ public class LiteralNode extends OperandoNode {
     public void generateCode(CodeGenerator cg) {
         if(token.getType() == TokenType.NUM) {
             cg.getTextSection().append("li $a0, ").append(token.getLexeme()).append("\n");
+        } else if (token.getType()==TokenType.PTRUE || token.getType() == TokenType.PFALSE) {
+            cg.getTextSection().append("li $a0, ").append(token.getLexeme().equals("true") ? 1 : 0).append("\n");
+        } else if (token.getType()==TokenType.CHAR) {
+            cg.getTextSection().append("li $a0, ").append((int)token.getLexeme().charAt(1)).append("\n");
+        } else if (token.getType()==TokenType.STRING) {
+            //TODO Revisar que funcione
+            String lexeme = token.getLexeme();
+            int length = lexeme.length();
+
+            cg.getTextSection().append("li $v0, 9\n");
+            cg.getTextSection().append("li $a0, ").append(length + 1).append("\n"); //Conseguir la longitud del coso
+            cg.getTextSection().append("syscall\n");
+            cg.getTextSection().append("move $a0, $v0\n"); // Store the address of the allocated memory in $t0
+
+            // Copy the string to the allocated memory
+            for (int i = 1; i <= length; i++) { // Start from 1 to skip the opening quote and end at length to skip the closing quote
+                char c = lexeme.charAt(i);
+                cg.getTextSection().append("li $t0, ").append((int) c).append("\n");
+                cg.getTextSection().append("sb $t0, ").append(i - 1).append("($a0)\n");
+            }
+
+            // Add the null terminator
+            cg.getTextSection().append("li $t1, \0\n");
+            cg.getTextSection().append("sb $t1, ").append(length).append("($t0)\n");
+
+
         }
-        //TODO: ver para otros casos
     }
 }
